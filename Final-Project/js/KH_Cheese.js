7172671e6e9b206321590191188bee0b5d6fac82
KayHolly <kayholly@iu.edu>
2025-05-08 23:56:03 -0400

    Things

--- Final-Project/js/KH_Cheese.js
+++ Final-Project/js/KH_Cheese.js
@@ -1,1 +1,12 @@
+const detailSectionRef = document.querySelector("#details");
 
+const winData = { 
+    "winners": [ 
+{ "name": "Roy Mitchell","image": "Assests/Cheese-Rolling-.jpg", "year": "1948" },
+{ "name": "Roy Mitchel","image": "Assests/Cheese-Rolling-.jpg", "year": "1948" },
+{ "name": "Roy Mitche","image": "Assests/Cheese-Rolling-.jpg", "year": "1948" },
+{ "name": "Roy Mitch","image": "Assests/Cheese-Rolling-.jpg", "year": "1948" },
+{ "name": "Roy Mitc","image": "Assests/Cheese-Rolling-.jpg", "year": "1948" },
+{ "name": "Roy Mit","image": "Assests/Cheese-Rolling-.jpg", "year": "1948" },
+]
+ },
